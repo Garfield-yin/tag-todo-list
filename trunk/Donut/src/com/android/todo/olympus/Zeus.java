5e1a0c66ac7b72454c64751677c749d190db8942
teominator <teominator@ce2904a8-9604-11dd-a59c-95bcd8c322ac>
2010-09-07 09:08:04 +0000

    Zeus is now implemented

---
+++ trunk/Donut/src/com/android/todo/olympus/Zeus.java
@@ -0,0 +1,46 @@
+package com.android.todo.olympus;
+
+import com.android.todo.TagToDoList;
+
+import android.os.Handler;
+import android.view.View;
+
+/**
+ * Zeus, the god of thunder. Instead of bugging users with explicit messages
+ * about app features, we can remind them in a subtle way that they're there.
+ * For example, a very short moment of invisibility might attract the user's
+ * attention and determine him to take action on the respective UI widget.
+ */
+public final class Zeus {
+  /**
+   * The time the app will wait before reminding the user
+   */
+  private final static long INITIAL_DELAY_TIME=5000;
+  
+  /**
+   * The duration of the view's invisibility
+   */
+  private final static long THUNDER_TIME=200;
+  
+  public final static void remind(final View v, final String key){
+    final Handler h=new Handler();
+    h.postDelayed(new Runnable(){
+      public void run() {
+          try{
+          if (TagToDoList.sPref.getInt(key, 0)<1){
+            v.setVisibility(View.INVISIBLE);
+            h.postDelayed(new Runnable(){
+              public void run() {
+                try{
+                v.setVisibility(View.VISIBLE);
+                }catch(Exception e){
+                }
+              }
+            }, THUNDER_TIME);
+          }
+          }catch(Exception e){
+          }
+      }
+    }, INITIAL_DELAY_TIME);
+  }
+}
